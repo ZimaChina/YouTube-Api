314cfa26dfeb84ea3b7cacbf26ce8c43fdfa6cb8
Fernando Duarte <108553217+ZimaChina@users.noreply.github.com>
2023-01-24 19:44:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from youtube_statistics import YTstats
 
-API_KEY = "" #there might be a reset
-channel_id = "@JazzAcademy"
+API_KEY = "AIzaSyCk4SF_hSaZK9U5H0wWflRvIgymFF69pb0" 
+channel_id = "UC8ycFlguVIToxRB34x3g1eQ"
 
 yt = YTstats(API_KEY, channel_id)
 yt.get_channel_stats()
+yt.get_channel_video_data()
+yt.dump()
